4804630269c11c1ee498ab7bf62b9be73ea4ade9
phoot <philippe.thauvin@free.fr>
2014-10-15 10:33:58 +0200

    Update ConnectedObjects.java

--- app/src/main/java/fr/oxilea/myhome/ConnectedObjects.java
+++ app/src/main/java/fr/oxilea/myhome/ConnectedObjects.java
@@ -6,8 +6,8 @@ package fr.oxilea.myhome;
 public class ConnectedObjects {
 
     // data structure: ObjectName | index | type (pulse 0 or On/off 1 | localIpAddress | local port | networkIpAddress | networkPort | localWifiName | devicePswd
-    String connectedObjectList [][]={{"Portail", "0", "0","192.168.2.23","8899","http://phoot.hd.free.fr","8899", "PhTNetwork","xxx"},
-            {"Prise", "1", "1","192.168.2.23","8899","http://phoot.hd.free.fr","8899", "PhTNetwork", "xxx"}};
+    String connectedObjectList [][]={   {"Portail", "0", "0","your.internet.ip","8899","yourpsw"},
+                                        {"Prise", "1", "1","your.internet.ip","889A", "yourpsw"}};
 
     /**
      * Constructor of the class.
